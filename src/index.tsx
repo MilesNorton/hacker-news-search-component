a039e2dfd544a3debd650f9818006fd9ad0014a3
miles <miles.norton1@hotmail.com>
2020-07-05 21:35:37 +1000

    added spa

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,19 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+// import * as React from "react";
+// import * as ReactDOM from "react-dom";
 
 import App from "./app/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Note that SingleSpaContext is a react@16.3 (if available) context that provides the singleSpa props
+import singleSpaReact from "single-spa-react";
+const reactLifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: App,
+});
+export const bootstrap = reactLifecycles.bootstrap;
+export const mount = reactLifecycles.mount;
+export const unmount = reactLifecycles.unmount;
